9bf7456a86e8f01f389bf9c4f833dc54f3c1b739
LeonardoPereira21 <leopereigon@gmail.com>
2023-01-07 02:20:43 -0300

    using hook on summary component

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,37 +1,36 @@
 import { SummaryCard, SummaryContainer } from "./styles";
-import{ArrowCircleDown, ArrowCircleUp, CurrencyDollar} from 'phosphor-react'
-export function Summary(){
-    return(
-        <SummaryContainer>
-            <SummaryCard>
-                <header>
-                    <span>Entradas</span>
-                    <ArrowCircleUp size={32} color="#00B37E"/>
-                </header>
-                <strong>
-                    R$ 17.400,00
-                </strong>
-            </SummaryCard>
+import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from "phosphor-react";
+import { useContext } from "react";
+import { TransactionsContext } from "../../contexts/TransactionsContext";
+import { useSummary } from "../../hooks/useSummary";
+export function Summary() {
+  const summary = useSummary();
 
-            <SummaryCard>
-                <header>
-                    <span>Saidas</span>
-                    <ArrowCircleDown size={32} color="#F75A68"/>
-                </header>
-                <strong>
-                    R$ 17.400,00
-                </strong>
-            </SummaryCard>
+  return (
+    <SummaryContainer>
+      <SummaryCard>
+        <header>
+          <span>Entradas</span>
+          <ArrowCircleUp size={32} color="#00B37E" />
+        </header>
+        <strong>{summary.income}</strong>
+      </SummaryCard>
 
-            <SummaryCard variant="green">
-                <header>
-                    <span>Total</span>
-                    <CurrencyDollar size={32} color="#FFF"/>
-                </header>
-                <strong>
-                    R$ 17.400,00
-                </strong>
-            </SummaryCard>
-        </SummaryContainer>
-    )
+      <SummaryCard>
+        <header>
+          <span>Saidas</span>
+          <ArrowCircleDown size={32} color="#F75A68" />
+        </header>
+        <strong>{summary.outcome}</strong>
+      </SummaryCard>
+
+      <SummaryCard variant="green">
+        <header>
+          <span>Total</span>
+          <CurrencyDollar size={32} color="#FFF" />
+        </header>
+        <strong>{summary.total}</strong>
+      </SummaryCard>
+    </SummaryContainer>
+  );
 }
